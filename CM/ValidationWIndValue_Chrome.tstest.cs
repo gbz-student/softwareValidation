a575832e7cf153650e88580be10fed841eca78a0
VOUGY <guillaume.vouilloz@outlook.com>
2019-10-18 11:51:39 +0200

    Correction

---
+++ CM/ValidationWIndValue_Chrome.tstest.cs
@@ -0,0 +1,59 @@
+using Telerik.TestStudio.Translators.Common;
+using Telerik.TestingFramework.Controls.KendoUI.Angular;
+using Telerik.TestingFramework.Controls.KendoUI;
+using Telerik.WebAii.Controls.Html;
+using Telerik.WebAii.Controls.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+using ArtOfTest.Common.UnitTesting;
+using ArtOfTest.WebAii.Core;
+using ArtOfTest.WebAii.Controls.HtmlControls;
+using ArtOfTest.WebAii.Controls.HtmlControls.HtmlAsserts;
+using ArtOfTest.WebAii.Design;
+using ArtOfTest.WebAii.Design.Execution;
+using ArtOfTest.WebAii.ObjectModel;
+using ArtOfTest.WebAii.Silverlight;
+using ArtOfTest.WebAii.Silverlight.UI;
+
+namespace Test_HotMaps___dev
+{
+
+    public class ValidationWIndValue_Chrome : BaseWebAiiTest
+    {
+        #region [ Dynamic Pages Reference ]
+
+        private Pages _pages;
+
+        /// <summary>
+        /// Gets the Pages object that has references
+        /// to all the elements, frames or regions
+        /// in this project.
+        /// </summary>
+        public Pages Pages
+        {
+            get
+            {
+                if (_pages == null)
+                {
+                    _pages = new Pages(Manager.Current);
+                }
+                return _pages;
+            }
+        }
+
+        #endregion
+        
+        // Add your test methods here...
+    
+        [CodedStep(@"Verify 'TextContent' 'Contains' '4.16 m/s' on 'x305MSTableCell'")]
+        public void ValidationWIndValue_CodedStep()
+        {
+            // Verify 'TextContent' 'Contains' '4.16 m/s' on 'x305MSTableCell'
+            Pages.Toolbox.x305MSTableCell.AssertContent().TextContent(ArtOfTest.Common.StringCompareType.Contains, "4.16 m/s");
+            
+        }
+    }
+}
